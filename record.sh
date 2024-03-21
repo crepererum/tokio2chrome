43cadfd1525a521b1bfb353f6fdffd0c2e885d7d
Marco Neumann <marco@crepererum.net>
2024-03-21 16:26:01 +0100

    refactor: make record script less resource hungry

--- record.sh
+++ record.sh
@@ -3,9 +3,9 @@
 set -eu -o pipefail
 
 exec perf record \
-    --call-graph=dwarf,65528 \
-    --event=cycles/freq=9999/ \
-    --event=instructions/freq=9999/ \
+    --call-graph=fp \
+    --event=cycles/freq=999/ \
+    --event=instructions/freq=999/ \
     --event=raw_syscalls:sys_enter \
     --event=raw_syscalls:sys_exit \
     --event=sdt_tokio:task_finish \
