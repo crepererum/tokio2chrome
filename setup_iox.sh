4c7297cb28b86c5b449f925cfd22815467b2d9af
Marco Neumann <marco@crepererum.net>
2024-03-21 16:25:09 +0100

    refactor: add new events to setup script, make it less verbose

--- setup_iox.sh
+++ setup_iox.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eux -o pipefail
+set -eu -o pipefail
 
 readonly IOX_PATH="target/quick-release/influxdb_iox"
 
@@ -11,11 +11,19 @@ perf buildid-cache --remove "$IOX_PATH"
 perf buildid-cache --add "$IOX_PATH"
 
 # register probes
-probes=( task_finish task_poll_begin task_poll_end task_blocking_begin task_blocking_end )
+probes=(
+    task_blocking_begin
+    task_blocking_end
+    task_finish
+    task_poll_begin
+    task_poll_end
+    task_schedule_start
+    task_start
+)
 for probe in "${probes[@]}"; do
     set +e
     perf probe --del="sdt_tokio:$probe"
     set -e
 
-    perf probe --verbose --add="sdt_tokio:$probe"
+    perf probe --add="sdt_tokio:$probe"
 done
